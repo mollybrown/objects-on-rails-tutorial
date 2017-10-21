61c4bde75fd59896e4c3b329d673ed9eea545e21
Molly Brown <mollyrachelbrown@gmail.com>
2017-10-21 16:02:16 -0700

    First commit, objects-on-rails tutorial

---
+++ spec/models/blog_spec.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require_relative '../../app/models/blog'
+
+describe Blog do
+
+  before do
+    @it = Blog.new
+  end
+
+  it "has no entries" do
+    @it.entries.must_be_empty
+  end
+
+  it "has multiple entries" do
+
+  end
+
+end
