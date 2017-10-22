bac1d7cc1c1550f798a38d02304d75f2beb9c19a
Molly Brown <mollyrachelbrown@gmail.com>
2017-10-21 17:17:49 -0700

    add post model and spec

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,7 @@
 class Blog
 
   attr_reader :entries
+  attr_writer :post_source
 
   def initialize
     @entries = []
@@ -13,5 +14,18 @@ class Blog
   def subtitle
     "The trusted source for drying paint news & opinion"
   end
+
+  def new_post
+    post_source.call.tap do |post|
+      post.blog = self
+    end
+  end
+
+  private
+    # unlike #method, #public_method respects public/private boundaries and
+    # will not generate a Method object for a private method.
+    def post_source
+      @post_source ||= Post.public_method(:new)
+    end
 
 end
